d05e63b1c73b69c3b73120e27e1ca0491b33be37
Robby Nelson <robertb.nelson@gmail.com>
2017-05-01 13:30:01 -0700

    Finish json loader

--- feebb/preprocessor.py
+++ feebb/preprocessor.py
@@ -2,12 +2,18 @@ import json
 
 class Preprocessor:
     def __init__(self):
+        self.reset()
+
+    def reset(self):
         self.number_elements = 0
         self.length_elements = []
         self.E_elements = []
         self.I_elements = []
+        self.loads = []
+        self.supports = []
 
-    def read_json(self, infile):
+    def load_json(self, infile):
+        self.reset()
         with open(infile) as json_model:
             model = json.load(json_model)
 
@@ -15,3 +21,9 @@ class Preprocessor:
         self.length_elements = [element['length'] for element in model['elements']]
         self.E_elements = [element['youngs_mod'] for element in model['elements']]
         self.I_elements = [element['moment_of_inertia'] for element in model['elements']]
+        for element in model['elements']:
+            for load in element['loads']:
+                load["element"] = element["element"]
+                self.loads.append(load)
+
+        self.supports = model['supports']
