270427924cf1e10a5af506dfd1d7e2eaa569e39c
Robby Nelson <robby@nelson-wilcox.com>
2017-06-22 09:49:16 -0700

    Add some file to look at convergance

---
+++ feebb/test.py
@@ -0,0 +1,33 @@
+from feebb import *
+import matplotlib.pyplot as plt
+
+pre = Preprocessor()
+pre.load_json('ex_json/test2.json')
+elems = [Element(elem) for elem in pre.elements]
+beam = Beam(elems, pre.supports)
+post = Postprocessor(beam, 100)
+print(max(post.interp('moment')))
+print(min(post.interp('moment')))
+plt.plot(post.interp('moment'))
+plt.show()
+
+pre = Preprocessor()
+pre.load_json('ex_json/test2m.json')
+elems = [Element(elem) for elem in pre.elements]
+beam = Beam(elems, pre.supports)
+post = Postprocessor(beam, 100)
+print(max(post.interp('moment')))
+print(min(post.interp('moment')))
+plt.plot(post.interp('moment'))
+plt.show()
+
+pre = Preprocessor()
+pre.load_json('ex_json/test2mm.json')
+elems = [Element(elem) for elem in pre.elements]
+beam = Beam(elems, pre.supports)
+post = Postprocessor(beam, 100)
+print(max(post.interp('moment')))
+print(min(post.interp('moment')))
+plt.plot(post.interp('moment'))
+plt.show()
+
